841b6c1b980a0616f65da187e160a933ba526aea
Mindaugas Kazlauskas <o948mx@gmail.com>
2020-10-17 15:28:08 +1100

    ignore size hints in tiled mode

--- config.def.h
+++ config.def.h
@@ -29,7 +29,6 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 /* key definitions */
 #define MODKEY Mod1Mask
